30eda00c81d99efd538b5e7d4e0e9b1aee5e0d08
Рыбина Лидия <lidiar@rambler.ru>
2025-09-27 16:11:11 +0500

    update test 2 task

--- src/main_function.py
+++ src/main_function.py
@@ -1,4 +1,3 @@
-from typing import Dict, List
 
 def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
     """Функция вычисляет стоимость товаров с учётом налога."""
@@ -21,36 +20,13 @@ def calculate_tax(price: float, tax_rate: float) -> float:
     """Функция вычисляет стоимость товаров с учётом налога."""
 
     if price < 0:
-        raise ValueError('Неверная цена')
+       raise ValueError('Неверная цена')
 
-    if 100 <= tax_rate < 0:
+    if tax_rate < 0 or tax_rate >= 100:
         raise ValueError('Неверный налоговый процент')
 
     tax = price * tax_rate / 100
     return price + tax
 
 
-
-
-
-#
-#
-# def sort_by_date(input_list: List[Dict], sorting: bool = True) -> List[Dict]:
-#     """
-#     возвращает список словарей, отсортированный по дате
-#     :param input_list: список словарей
-#     :param sorting: необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)
-#     :return: list отсортированный по дате список словарей
-#     """
-#     return sorted(input_list, key=lambda current_dict: current_dict["date"], reverse=sorting)
-#
-
-# lists = [
-#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-# ]
-#
-# # print(filter_by_state(lists, 'CANCELED'))
-# print(sort_by_date(lists, False))
+print(calculate_taxes([100, 102], tax_rate=-20))
